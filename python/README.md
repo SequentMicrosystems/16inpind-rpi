5a81eccc4d7b9212282cd2b1d14ec58bf29d2732
Vladutu Cristian-Vlad <cristian.vlad.vladutu@gmail.com>
2023-12-01 22:23:45 +0000

    Restructured and ready for pypi.

--- python/README.md
+++ python/README.md
@@ -5,28 +5,10 @@ This is the python library to control [16- INPUTS card for Raspberry Pi](https:/
 ## Install
 
 ```bash
-~$ sudo apt-get update
-~$ sudo apt-get install build-essential python-pip python-dev python-smbus git
-~$ git clone https://github.com/SequentMicrosystems/16inpind-rpi.git
-~$ cd 16inpind-rpi/python/16inpind/
-~/16inpind-rpi/python/16inpind$ sudo python setup.py install
+sudo pip install SM16inpind
 ```
 
-for Python 3.x usge replace last line with:
-```bash
-~/16inpind-rpi/python/16inpind$ sudo python3 setup.py install
-```
-
-## Update
-
-```bash
-~$ cd 16inpind-rpi/
-~/16inpind-rpi$ git pull
-~$ cd 16inpind-rpi/python/16inpind/
-~/16inpind-rpi/python/16inpind$ sudo python setup.py install
-```
-
-## Usage 
+## Usage
 
 Now you can import the lib16inpind library and use its functions. To test, read the channel 1:
 
@@ -43,3 +25,26 @@ Type "help", "copyright", "credits" or "license" for more information.
 ## [Documentation](https://github.com/SequentMicrosystems/16inpind-rpi/blob/master/python/16inpind/README.md). 
 
 This library works with both Python2 and Python3
+# lib16inpind
+
+## Functions
+
+## readCh(stack, channel)
+
+ - Card stack level [0..7]
+
+ - channel Selected channel to be read [1..16]
+ 
+ - Return one channel input state 0 / 1
+ 
+## readAll(stack)
+
+ - Card stack level [0..7]
+
+ - Return all channels inputs states as 16 bits unsigned int [0..65535]
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ python/setup.py
@@ -0,0 +1,44 @@
+#python2 setup.py sdist bdist_wheel
+
+# For testing
+#twine upload --repostitory testpypi dist/*
+#pip install --index-url https://test.pypi.org/simple/ --no-deps multiio
+
+# For release
+#twine upload dist/*
+#pip install multiio
+
+
+#with open("README.md", 'r') as f:
+#    long_description = f.read()
+
+from setuptools import setup, find_packages
+setup(
+    name='SM16inpind',
+    packages=find_packages(),
+    version='1.0.0',
+    license='MIT',
+    description='Library to control 16inpind Automation Card',
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    author='Sequent Microsystems',
+    author_email='olcitu@gmail.com',
+    url='https://sequentmicrosystems.com',
+    #keywords=['industrial', 'raspberry', 'power', '4-20mA', '0-10V', 'optoisolated'],
+    install_requires=[
+        "smbus2",
+        ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    )
